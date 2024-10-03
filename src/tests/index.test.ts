968ea507d1d5057709908d749c7bb52906b5b822
Ayan <ayan2572006@gmail.com>
2024-10-03 15:08:31 +0530

    testing

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -6,4 +6,19 @@ describe("sum", () => {
     const finalAnswer = sum(1, 2);
     expect(finalAnswer).toBe(3);
   });
+
+  it("should add 2 + 2 to equal 4", () => {
+    const finalAnswer = sum(2, 2);
+    expect(finalAnswer).toBe(4);
+  });
+
+  it("should add 3 + 2 to equal 5", () => {
+    const finalAnswer = sum(3, 2);
+    expect(finalAnswer).toBe(5);
+  });
+
+  it("should add 4 + 2 to equal 6", () => {
+    const finalAnswer = sum(4, 2);
+    expect(finalAnswer).toBe(6);
+  });
 });
